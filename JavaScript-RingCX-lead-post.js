3a558fb528003ca79af477f6a4b53db57881c529
mmusikevinmotsumi <mmusikevinmotsumi@gmail.com>
2024-10-08 05:36:25 -0400

    Update JavaScript-RingCX-lead-post.js

--- JavaScript-RingCX-lead-post.js
+++ JavaScript-RingCX-lead-post.js
@@ -8,29 +8,58 @@ const duplicateHandling= inputData.duplicateHandling;
 const listState= inputData.listState;
 const timeZoneOption= inputData.timeZoneOption;
 const leadPhone= inputData.leadPhone;
-const externId= inputData.externId;
-const firstName= inputData.firstName;
-const lastName= inputData.lastName;
-const address1= inputData.address1;
-const city= inputData.city;
-const state= inputData.state;
-const zip= inputData.zip;
-const email= inputData.email;
-const auxData1= inputData.auxData1;
-const auxData2= inputData.auxData2;
-const auxData3= inputData.auxData3;
-const auxData4= inputData.auxData4;
-const auxData5= inputData.auxData5;
-
+const externId= inputData.externId || "";
+const firstName= inputData.firstName || "";
+const lastName= inputData.lastName || "";
+const address1= inputData.address1 || "";
+const city= inputData.city || "";
+const state= inputData.state || "";
+const zip= inputData.zip || null;
+const email= inputData.email || "";
+const auxData1= inputData.auxData1 || "";
+const auxData2= inputData.auxData2 || "";
+const auxData3= inputData.auxData3 || "";
+const auxData4= inputData.auxData4 || "";
+const auxData5= inputData.auxData5 || "";
 
 const url = `${baseURL}/api/v1/admin/accounts/${accountId}/campaigns/${campaignId}/leadLoader/direct`;
 
 
 const headers = {
   'Content-Type': 'application/json',
-  'Authorization': `Bearer ${accessToken}`
+  'X-Auth-Token': `${accessToken}`
 };
 
+const data = {
+  "description": description,
+  "dialPriority": dialPriority,
+  "duplicateHandling": duplicateHandling,
+  "listState": listState,
+  "timeZoneOption": timeZoneOption,
+  "uploadLeads": [
+    {
+      "email": email,
+      "leadPhone": leadPhone,
+      "firstName": firstName,
+      "lastName": lastName,
+      "address1": address1,
+      "externId": externId,
+      "city": city,
+      "state": state,
+      "zip": zip,
+      "auxData1": auxData1,
+      "auxData2": auxData2,
+      "auxData3": auxData3,
+      "auxData4": auxData4,
+      "auxData5": auxData5
+    }
+  ]
+};
 
-
-output = [{id: 123, hello: "world"}];
+const res = await fetch(url, {
+  method: 'POST',
+  headers: headers,
+  body: JSON.stringify(data)
+})
+const body = await res.text();
+return {result: body};
